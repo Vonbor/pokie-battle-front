8d66dcb69824778a42228927e25fe95d5bfe0b3e
nasrul <imsohad@gmail.com>
2020-12-02 17:09:32 +0100

    components and structure

---
+++ src/Pokemon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export default function Pokemon () {
+    return (
+
+        
+       
+    )
+
+}
