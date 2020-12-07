25fd9503776224acf992cf93e1af56a755df0b38
lidiyacheb <lidia.saulite@gmail.com>
2020-12-07 16:54:41 +0100

    add pokemon + detail components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import './App.css';
 import React from 'react';
-import { Route, Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Pokemon from "./Pokemon";
-
+import { Switch, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Pokemon from './Pokemon';
+import Details from './Details';
 function App() {
-  const [data, setData] = useState([]);
-  
-  
-  useEffect(()=> {
-    fetch("https://pokie-app.herokuapp.com/pokemon")
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('https://pokie-app.herokuapp.com/pokemon')
+            .then((res) => res.json())
+            .then((res) => {
+                setData(res);
+                console.log(res);
+            });
+    }, []);
 
-        .then(res => res.json())
-        .then(res => {setData(res)
-        console.log(res)}
-        );
-
-  }, []);
-
-  return (
-
-    
-
-    <div className="App">
-      <Pokemon data={data} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <main>
+                <h1 id="main-header">POKEFIGHT</h1>
+                <Switch>
+                    <Route exact path="/">
+                        <Pokemon data={data} />
+                    </Route>
+                    <Route path="/:id">
+                        <Details data={data} />
+                    </Route>
+                </Switch>
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,16 +1,61 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/fontawesome-free-solid';
+import './Pokemon.css';
 
-import { Route, Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+export default function Pokemon({ data }) {
+    if (data.length) {
+        return (
+            <div id="app">
+                {data.map((poki) => {
+                    return (
+                        <div class="poke-card" id="{poki.name}">
+                            <div class="flexy">
+                                <span class="card-id">#{poki.id}</span>
+                                <span class="card-hp">
+                                    HP {poki.base.HP}
+                                    <i
+                                        id="poke-hp"
+                                        class="fa fa-heart"
+                                        aria-hidden="true"
+                                    >
+                                        <FontAwesomeIcon icon={faHeart} />
+                                    </i>
+                                </span>
+                            </div>
+                            <h1 class="card-name">{poki.name.english}</h1>
+                            <img
+                                class="card-image"
+                                src="poki.png"
+                                alt={poki.name.english}
+                            />
+                            <span class="card-details">
+                                Type:{' '}
+                                {poki.type.map((elem) => {
+                                    return (
+                                        <span className="pokiType">{elem}</span>
+                                    );
+                                })}
+                            </span>
+                            <span>
+                                {Object.keys(poki.base).map((key) => {
+                                    return (
+                                        <span className="pokiBase">
+                                            {key}: {poki.base[key]},
+                                        </span>
+                                    );
+                                })}
+                            </span>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    } else {
+        return <p className="spinner">LOADING...</p>;
+    }
+}
 
-export default function Pokemon ({data}) {
-    return (
-        <div className="wrapper"> {data.map((poki) => 
-            {return(<div>{poki}</div>)})}
-        </div>
-
-        
-       
-    )
-
+{
+    /* <Link to={`${poki.id}`}><a class="btn">Detail</a></Link> */
 }
