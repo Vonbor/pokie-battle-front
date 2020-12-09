20ec21390815d43136d2f4cf6ac965ab6bf8c79a
lidiyacheb <lidia.saulite@gmail.com>
2020-12-09 13:31:10 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import './App.css';
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+
 import Pokemon from './Pokemon';
 import Details from './Details';
+import Info from './Info';
 function App() {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        fetch('https://pokie-app.herokuapp.com/pokemon')
-            .then((res) => res.json())
-            .then((res) => {
-                setData(res);
-                console.log(res);
-            });
-    }, []);
-
     return (
         <div className="App">
             <main>
                 <h1 id="main-header">POKEFIGHT</h1>
                 <Switch>
                     <Route exact path="/">
-                        <Pokemon data={data} />
+                        <Pokemon />
                     </Route>
-                    <Route path="/:id">
-                        <Details data={data} />
+                    <Route exact path="/pokemon/:id">
+                        <Details />
+                    </Route>
+                    <Route exact path="/pokemon/:id/:info">
+                        <Info />
                     </Route>
                 </Switch>
             </main>
